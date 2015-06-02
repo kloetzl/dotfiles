846e8e8b5991ddcc6a4312d7b69ce69087d236c9
Fabian Klötzl <fabian@kloetzl.info>
2015-06-02 08:21:24 +0200

    adds .gitconfig

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ echo "ZDOTDIR=$PWD" >> ~/.zshenv
 
 # ignore changes to local file
 git update-index --assume-unchanged zsh/lib/local.zsh
+
+# setup global .gitconfig file
+# For the moment I am using this, a symlink might also be possible.
+cat .gitconfig >> ~/.gitconfig
