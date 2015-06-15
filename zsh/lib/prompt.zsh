2a8d205e2483d2389e16bbc87ceb769dc9da8b1f
Fabian Klötzl <fabian@kloetzl.info>
2015-06-15 19:23:25 +0200

    comment out various (useless) lines

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -1,7 +1,9 @@
+# TODO: Check if this file actually does something useful.
+
 # autoload -U add-zsh-hook
 autoload -Uz vcs_info
 
-# # enable VCS systems you use
+# enable VCS systems you use
 zstyle ':vcs_info:*' enable git svn
 zstyle ':vcs_info:*:prompt:*' check-for-changes true
 
@@ -12,14 +14,15 @@ promptinit
 
 # Look at http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Version-Control-Information
 # for mor options
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '*'   # display this when there are unstaged changes
-zstyle ':vcs_info:*' stagedstr '+'  # display this when there are staged changes
-zstyle ':vcs_info:*' actionformats '[%b%|%a%c%u%]%f'
-zstyle ':vcs_info:*' formats ':%b%c%u%f'
+# zstyle ':vcs_info:*' check-for-changes true
+# zstyle ':vcs_info:*' unstagedstr '*'   # display this when there are unstaged changes
+# zstyle ':vcs_info:*' stagedstr '+'  # display this when there are staged changes
+# zstyle ':vcs_info:*' actionformats '[%b%|%a%c%u%]%f'
+# zstyle ':vcs_info:*' formats ':%b%c%u%f'
 
 
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
-precmd () { vcs_info }
+# zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+# precmd () { vcs_info }
 
-PROMPT='[%n@%m %~${vcs_info_msg_0_}] %{$reset_color%}'
+# Not sure if the following line is of any use at all
+# PROMPT='[%n@%m %~${vcs_info_msg_0_}] %{$reset_color%}'
