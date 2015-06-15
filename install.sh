953aee0301c07d2905e0333f0d359e5c7081ea72
Fabian Klötzl <fabian@kloetzl.info>
2015-06-15 21:40:04 +0200

    hide name@machine on localhost

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 echo "ZDOTDIR=$PWD" >> ~/.zshenv
 
-# ignore changes to local file
-git update-index --assume-unchanged zsh/lib/local.zsh
-
 # setup global .gitconfig file
 # For the moment I am using this, a symlink might also be possible.
 cat .gitconfig >> ~/.gitconfig
+
+echo "DEFAULT_USER=$(whoami)" >> zsh/lib/local.zsh
+
+# ignore changes to local file
+git update-index --assume-unchanged zsh/lib/local.zsh
