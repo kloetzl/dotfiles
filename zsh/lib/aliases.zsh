aaa32a0d91e29ab167868158ece33523451271f4
Fabian Klötzl <fabian@kloetzl.info>
2015-11-23 20:58:10 +0100

    add aliases for sanitizers

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,3 +1,17 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
+
+function(){
+	local PF="make CC=clang CXX=clang++"
+	local COMMON="-fno-omit-frame-pointer -O0 -g -ggdb"
+	local MSANFLAGS="$COMMON -fsanitize=memory -fsanitize-memory-track-origins=2"
+	local ASANFLAGS="$COMMON -fsanitize=address"
+	local USANFLAGS="$COMMON -fsanitize=undefined"
+	local ISANFLAGS="$COMMON -fsanitize=integer"
+
+	alias msan="$PF CFLAGS=\"$MSANFLAGS\" CXXFLAGS=\"$MSANFLAGS -std=c++11\""
+	alias asan="$PF CFLAGS=\"$ASANFLAGS\" CXXFLAGS=\"$ASANFLAGS -std=c++11\""
+	alias usan="$PF CFLAGS=\"$USANFLAGS\" CXXFLAGS=\"$USANFLAGS -std=c++11\""
+	alias isan="$PF CFLAGS=\"$ISANFLAGS\" CXXFLAGS=\"$ISANFLAGS -std=c++11\""
+}
